d472dbaa612a88953fe555076400fd5f2262e6b1
Sagar Saxena <sagardsaxena@gmail.com>
2020-01-26 23:47:17 -0500

    Basic Drone Strat

--- src/droneRush/Drone.java
+++ src/droneRush/Drone.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class Drone extends Unit {
 	String job;
-	
+	MapLocation water;
+
 	public Drone(RobotController rc) {
 		super(rc);	
 	}
@@ -59,6 +60,84 @@ public class Drone extends Unit {
 	}
 	
 	private void runAttack() {
-		while (true);
+		MapLocation target = null;
+		MapLocation water = null;
+		Team opp = rc.getTeam().opponent();
+		int rob_id = -1;
+		boolean carrying = false;
+		Direction heading = null;
+
+		while (true) {
+			try {
+				MapLocation myLoc = rc.getLocation();
+				
+				System.out.println("Water Status: " + water);
+				if (water == null)
+					for (Direction dir : directions)
+						if (rc.canSenseLocation(myLoc.add(dir)) &&  rc.senseFlooding(myLoc.add(dir)))
+							water = myLoc.add(dir);				
+				
+				System.out.println("Target Status: " + target);
+				if (target == null) {
+					RobotInfo[] info = rc.senseNearbyRobots();
+					for (RobotInfo rob : info)
+						if (rob.getTeam() == opp && rob.getType() == RobotType.MINER) {
+							target = rob.getLocation();
+							rob_id = rob.getID();
+						}
+				}
+			
+				System.out.println("Carrying Status: " + carrying);	
+				if (rc.canPickUpUnit(rob_id)) {
+					rc.pickUpUnit(rob_id);
+					carrying = true;
+				}
+
+				if (carrying)
+					for (Direction dir : directions)
+						if (rc.canSenseLocation(myLoc.add(dir)) && rc.senseFlooding(myLoc.add(dir)) 
+							&& rc.canDropUnit(dir)) {
+							rc.dropUnit(dir);
+							target = null;
+							carrying = false;
+						}
+				
+				if (carrying && water != null) {
+					if (!pathFindTo(water, myLoc.distanceSquaredTo(water) * 2, true, "Adj"))
+						water = null;
+
+				} else if (!carrying && target != null) {
+					if (!pathFindTo(target, myLoc.distanceSquaredTo(target) * 2, true, "Adj"))
+						target = null;
+
+				} else {
+					if (heading == null) 
+						heading = randomValidDirection();
+					System.out.println("Heading Status: " + heading);
+					
+					if (!pathFindTo(myLoc.add(heading).add(heading), 3, true, "Adj"))
+						heading = null;
+				}
+
+			} catch (GameActionException e) {
+				System.out.println("Failure: Drone.runAttack()\nFailed to attack miners");
+			}
+		}
 	}
+	
+	protected Direction randomValidDirection() {
+    	ArrayList<Direction> list = new ArrayList<Direction>();
+        for (Direction dir : directions) 
+			if (rc.canMove(dir)) list.add(dir);  
+		
+ 		if (list.isEmpty()) {
+			System.out.println("Oh no I am stuck"); 
+			return randomDirection();
+		} else
+			return list.get((int) (Math.random() * list.size()));	
+    }
+
+	protected Direction randomDirection() {
+        return directions.get((int) (Math.random() * directions.size()));
+    }
 }
